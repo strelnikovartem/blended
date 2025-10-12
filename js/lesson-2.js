2c0187114dc0d8ed8f29eec85381364b14b4877f
Artem Strelnikov <piramida1237@gmail.com>
2025-10-12 20:27:10 +0200

    20:26

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -31,16 +31,16 @@
 // Якщо ім'я є в масиві – виводить повідомлення через alert: "Welcome, <name>!"
 // Якщо ім'я відсутнє – виводить повідомлення: "User not found".
 
+const logins = ['Peter', 'John', 'Igor', 'Sasha'];
+
 function checkLogin(array) {
+  const name = prompt('Enter your name');
   for (const item of array) {
-    const name = prompt('Enter your name');
     if (item === name) {
-      console.log(item);
+      alert(`Welcome, ${name}!`);
     }
   }
 }
-
-const logins = ['Peter', 'John', 'Igor', 'Sasha'];
 
 checkLogin(logins);
 
