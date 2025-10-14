83e9b7f714f38b4af18f66c94ea46c3eb731dd54
Artem Strelnikov <piramida1237@gmail.com>
2025-10-14 10:19:47 +0200

    clear

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -97,19 +97,3 @@
 
 // console.log(add(1, 2, 3));
 // add(10, 20, 30, 40);
-
-function fnA() {
-  console.log('fnA');
-}
-
-function fnB() {
-  console.log('fnB');
-}
-
-function fnC() {
-  console.log('fnC');
-  fnA();
-  fnB();
-}
-
-fnC();
