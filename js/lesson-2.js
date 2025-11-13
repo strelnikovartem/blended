45e3142fc8d8600c04ee97302fc8ad1cd92657f3
Artem Strelnikov <piramida1237@gmail.com>
2025-11-13 19:40:19 +0100

    forEach

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -261,3 +261,15 @@
 //!form
 
 //!call-back
+
+const numbers = [5, 10, 15, 20, 25];
+
+// Класичний for
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(`Index ${i}, value ${numbers[i]}`);
+}
+
+// Перебираючий метод forEach
+numbers.forEach(function (number, index) {
+  console.log(`Index ${index}, value ${number}`);
+});
