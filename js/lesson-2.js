969d43ee757d0fc3539a14b67671e2a9629fc9ea
Artem Strelnikov <piramida1237@gmail.com>
2025-11-13 19:36:42 +0100

    clear

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -261,18 +261,3 @@
 //!form
 
 //!call-back
-
-function greet(name) {
-  console.log(`Welcome ${name}!`);
-}
-
-function notify(name) {
-  console.log(`Dear ${name}, your room will be ready in 30 minutes`);
-}
-
-function registerGuest(name, callback) {
-  console.log(`Registering ${name}!`);
-  callback(name);
-}
-
-registerGuest('Mango', notify);
