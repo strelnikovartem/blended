8bd46e06cc5db68d359e3cbcf4b5beccbbe4f373
Artem Strelnikov <piramida1237@gmail.com>
2025-11-21 21:38:50 +0100

    adobe

--- js/task-2.js
+++ js/task-2.js
@@ -4,6 +4,11 @@ function calc(a, b) {
   return a + b;
 }
 
-function calc1(a, b) {}
+function calc1(a, b) {
+  const str = 'wow';
+  console.log(str);
+  return a - b;
+}
 
 console.log(calc(1, 2));
+console.log(calc1(10, 2));
