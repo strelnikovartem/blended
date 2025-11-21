728911125c26566df5d4d9ce431e26d388423c6a
Artem Strelnikov <piramida1237@gmail.com>
2025-11-21 21:42:38 +0100

    add_fanc_fna

--- js/task-2.js
+++ js/task-2.js
@@ -4,10 +4,8 @@ function calc(a, b) {
   return a + b;
 }
 
-function calc1(a, b) {
-  const str = 'wow';
-  console.log(str);
-  return a - b;
+function fnA(d, f) {
+  return d + f;
 }
 
 console.log(calc(1, 2));
