bf6d21bcc0e94de8bc0e8eb34926ee03f1f9f6ae
Artem Strelnikov <piramida1237@gmail.com>
2025-11-21 22:06:20 +0100

    dddfff

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,7 @@
 function each(array, callback) {}
 
-console.log(each([24, 61, 13, 33], funtion(value){}));
+console.log(
+  each([24, 61, 13, 33], function (value) {
+    return value * 2;
+  })
+);
