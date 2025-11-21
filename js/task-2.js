b26af49106402c1df40225e232a38aa814b97e2c
Artem Strelnikov <piramida1237@gmail.com>
2025-11-21 22:08:32 +0100

    item[]

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,8 @@
-function each(array, callback) {}
+function each(array, callback) {
+  for (const item of array) {
+    console.log(item);
+  }
+}
 
 console.log(
   each([24, 61, 13, 33], function (value) {
