98a019d01eba7b8a9e22123256d30d78e19c7eb9
Artem Strelnikov <piramida1237@gmail.com>
2025-11-22 14:57:05 +0100

    sdf

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,5 @@
-function foo(a, b, c) {
-  return a + b + c;
-}
-
-const arrowFoo = (...a) => {
+const arrowFoo = a => {
   return a;
 };
 
-console.log(foo(1, 2, 3));
-console.log(arrowFoo(2, 3, 4));
+console.log(arrowFoo(2));
