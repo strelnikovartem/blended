76a38fefe6108a38f32129b8d461ba606a6451c0
Artem Strelnikov <piramida1237@gmail.com>
2025-11-23 12:41:13 +0100

    clear

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,1 @@
-const arr = (...args) => {
-  let total = 0;
-  args.forEach(item => {
-    total += item;
-  });
 
-  return total;
-};
-
-console.log(arr(1, 2, 3, 4));
-console.log(arr(10, 20, 30));
-console.log(arr(12, 34, 45, 67, 89));
