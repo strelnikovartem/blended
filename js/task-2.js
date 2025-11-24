8bd3114c75c5c8e0f54c89fc8c2de53a77f6c952
Artem Strelnikov <piramida1237@gmail.com>
2025-11-24 10:45:36 +0100

    24.11

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,1 @@
-const players = [
-  { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
-  { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
-  { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
-  { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
-  { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
-];
-
-const playerIdToUpdate = 'player-3';
-
-const newArray = player => {
-  if (player.name === playerIdToUpdate) {
-    return {
-      ...player,
-      player.timePlayed += 100,
-    }
-  }
-}
+const array = [5, 10, 15, 20, 25];
