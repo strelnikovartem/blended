b5b0f968effff15cbb486741cebf103f84b47dd3
Artem Strelnikov <piramida1237@gmail.com>
2025-11-25 15:59:03 +0100

    add_players

--- js/task-2.js
+++ js/task-2.js
@@ -82,3 +82,11 @@
 // ];
 
 // const numbers = [1, 10, 20, 30, 40];
+
+const players = [
+  { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+  { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+  { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+  { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+  { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+];
