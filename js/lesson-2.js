0dc5aaaaa2749fcd07e27340457757fc17ed1a90
Artem Strelnikov <piramida1237@gmail.com>
2025-11-26 15:38:37 +0100

    clear

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -278,15 +278,4 @@
 
 const newArray = [10, 20, 30, 5, 40, 7, 50];
 
-function sortedNumbers(arr) {
-  for (const number of arr) {
-    if (number % 2 === 0) {
-      console.log('2%', number);
-      continue;
-    } else {
-      console.log('1%', number);
-    }
-  }
-}
-
 sortedNumbers(newArray);
