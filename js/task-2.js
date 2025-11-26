82ea41ff4d79aa2d1e503ec118714a5d94a41b63
Artem Strelnikov <piramida1237@gmail.com>
2025-11-26 09:47:31 +0100

    26.11

--- js/task-2.js
+++ js/task-2.js
@@ -1,99 +1,105 @@
-// const allCars = [
-//   {
-//     make: 'Honda',
-//     model: 'CR-V',
-//     type: 'suv',
-//     amount: 14,
-//     price: 24045,
-//     onSale: true,
-//   },
-//   {
-//     make: 'Honda',
-//     model: 'Accord',
-//     type: 'sedan',
-//     amount: 2,
-//     price: 22455,
-//     onSale: true,
-//   },
-//   {
-//     make: 'Mazda',
-//     model: 'Mazda 6',
-//     type: 'sedan',
-//     amount: 8,
-//     price: 24195,
-//     onSale: false,
-//   },
-//   {
-//     make: 'Mazda',
-//     model: 'CX-9',
-//     type: 'suv',
-//     amount: 7,
-//     price: 31520,
-//     onSale: true,
-//   },
-//   {
-//     make: 'Toyota',
-//     model: '4Runner',
-//     type: 'suv',
-//     amount: 19,
-//     price: 34210,
-//     onSale: false,
-//   },
-//   {
-//     make: 'Toyota',
-//     model: 'Sequoia',
-//     type: 'suv',
-//     amount: 16,
-//     price: 45560,
-//     onSale: false,
-//   },
-//   {
-//     make: 'Toyota',
-//     model: 'Tacoma',
-//     type: 'truck',
-//     amount: 4,
-//     price: 24320,
-//     onSale: true,
-//   },
-//   {
-//     make: 'Ford',
-//     model: 'F-150',
-//     type: 'truck',
-//     amount: 11,
-//     price: 27110,
-//     onSale: true,
-//   },
-//   {
-//     make: 'Ford',
-//     model: 'Fusion',
-//     type: 'sedan',
-//     amount: 13,
-//     price: 22120,
-//     onSale: true,
-//   },
-//   {
-//     make: 'Ford',
-//     model: 'Explorer',
-//     type: 'suv',
-//     amount: 6,
-//     price: 31660,
-//     onSale: false,
-//   },
-// ];
+const allCars = [
+  {
+    make: 'Honda',
+    model: 'CR-V',
+    type: 'suv',
+    amount: 14,
+    price: 24045,
+    onSale: true,
+  },
+  {
+    make: 'Honda',
+    model: 'Accord',
+    type: 'sedan',
+    amount: 2,
+    price: 22455,
+    onSale: true,
+  },
+  {
+    make: 'Mazda',
+    model: 'Mazda 6',
+    type: 'sedan',
+    amount: 8,
+    price: 24195,
+    onSale: false,
+  },
+  {
+    make: 'Mazda',
+    model: 'CX-9',
+    type: 'suv',
+    amount: 7,
+    price: 31520,
+    onSale: true,
+  },
+  {
+    make: 'Toyota',
+    model: '4Runner',
+    type: 'suv',
+    amount: 19,
+    price: 34210,
+    onSale: false,
+  },
+  {
+    make: 'Toyota',
+    model: 'Sequoia',
+    type: 'suv',
+    amount: 16,
+    price: 45560,
+    onSale: false,
+  },
+  {
+    make: 'Toyota',
+    model: 'Tacoma',
+    type: 'truck',
+    amount: 4,
+    price: 24320,
+    onSale: true,
+  },
+  {
+    make: 'Ford',
+    model: 'F-150',
+    type: 'truck',
+    amount: 11,
+    price: 27110,
+    onSale: true,
+  },
+  {
+    make: 'Ford',
+    model: 'Fusion',
+    type: 'sedan',
+    amount: 13,
+    price: 22120,
+    onSale: true,
+  },
+  {
+    make: 'Ford',
+    model: 'Explorer',
+    type: 'suv',
+    amount: 6,
+    price: 31660,
+    onSale: false,
+  },
+];
+
+const getModelsOnSale = allCars
+  .filter(car => car.onSale)
+  .toSorted((a, b) => a.price - b.price)
+
+  .map(car => {
+    return {
+      model: car.model,
+      price: car.price,
+    };
+  });
+
+console.table(getModelsOnSale);
 
 // const numbers = [1, 10, 20, 30, 40];
 
-const players = [
-  { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
-  { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
-  { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
-  { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
-  { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
-];
-
-const allTimePlayed = players.reduce(
-  (acc, player) => acc + player.timePlayed,
-  0
-);
-
-console.log(allTimePlayed);
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+// ];
