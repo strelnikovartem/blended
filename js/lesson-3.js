91f19060f49cbd7fe3ea3517dac034668fbc287c
Artem Strelnikov <piramida1237@gmail.com>
2025-12-01 21:03:12 +0100

    add+task-3

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -14,12 +14,28 @@
 // з масивів values кожного об'єкту, збережених в одному масиві.
 // Очікуваний результат: [1, 2, 3, 4, 5, 6, 7, 8, 9].
 
-const data = [
-  { id: 1, values: [1, 2, 3] },
-  { id: 2, values: [4, 5, 6] },
-  { id: 3, values: [7, 8, 9] },
+// const data = [
+//   { id: 1, values: [1, 2, 3] },
+//   { id: 2, values: [4, 5, 6] },
+//   { id: 3, values: [7, 8, 9] },
+// ];
+
+// const values = arr => arr.flatMap(item => item.values);
+
+// console.log(values(data));
+
+//*task-3
+
+// Дано масив об'єктів. 
+// Перевірте, чи є хоча б один об'єкт з віком менше 20 років. 
+// Очікуваний результат: true.
+
+const people = [
+ { name: 'John', age: 27 },
+ { name: 'Jane', age: 31 },
+ { name: 'Bob', age: 19 },
 ];
 
-const values = arr => arr.flatMap(item => item.values);
+const 
 
-console.log(values(data));
+
