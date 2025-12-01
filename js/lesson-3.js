c55502e6c2a1f7680f8584e6b10e757852526c29
Artem Strelnikov <piramida1237@gmail.com>
2025-12-01 20:40:10 +0100

    add_fin_task-2

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -4,6 +4,22 @@
 // квадрати кожного елементу вхідного масиву.
 // Очікуваний результат: [1, 4, 9, 16, 25].
 
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 
-console.log(numbers.map(num => num ** 2));
+// console.log(numbers.map(num => num ** 2));
+
+//?task-2
+
+// Дано масив об'єктів. Створіть новий масив, що містить всі значення
+// з масивів values кожного об'єкту, збережених в одному масиві.
+// Очікуваний результат: [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+const data = [
+  { id: 1, values: [1, 2, 3] },
+  { id: 2, values: [4, 5, 6] },
+  { id: 3, values: [7, 8, 9] },
+];
+
+const values = arr => arr.flatMap(item => item.values);
+
+console.log(values(data));
