3e306ee06c4ded4eb9685e4b28b9ae09926086bf
Artem Strelnikov <piramida1237@gmail.com>
2025-12-02 20:56:42 +0100

    fin task-7

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -88,8 +88,20 @@
 // Відсортуйте масив чисел [4, 2, 5, 1, 3]
 // у порядку зростання. Очікуваний результат: [1, 2, 3, 4, 5].
 
-const numbersArray = [4, 2, 5, 1, 3];
+// const numbersArray = [4, 2, 5, 1, 3];
 
-const sortedArray = arr => arr.toSorted((a, b) => a - b);
+// const sortedArray = arr => arr.toSorted((a, b) => a - b);
 
-console.log(sortedArray(numbersArray));
+// console.log(sortedArray(numbersArray));
+
+//!task-7
+
+// Відсортуйте масив рядків ["banana", "orange", "apple", "pear"]
+//  у порядку алфавіту.
+// Очікуваний результат: ["apple", "banana", "orange", "pear"].
+
+// const stringArray = ['banana', 'orange', 'apple', 'pear'];
+
+// const stringSort = arr => arr.toSorted((a, b) => a.localeCompare(b));
+
+// console.log(stringSort(stringArray));
