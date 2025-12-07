a1d80f7049f82805c69a484822742b09a63cfe72
Artem Strelnikov <piramida1237@gmail.com>
2025-12-07 17:05:26 +0100

    logistic

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -188,3 +188,13 @@
 //   .getResult(); // Отримуємо результат: 24
 
 // console.log(result); // 24
+
+const baseSalary = 30000;
+const overtime = 10;
+const rate = 20;
+
+const getWage = (base, overtime, rate) => {
+  return base + overtime * rate;
+};
+
+console.log(getWage(baseSalary, overtime, rate));
