0811955cddfc220f00af492473641db72584714d
Artem Strelnikov <piramida1237@gmail.com>
2025-12-07 17:26:21 +0100

    add _class

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -189,12 +189,4 @@
 
 // console.log(result); // 24
 
-const baseSalary = 30000;
-const overtime = 10;
-const rate = 20;
-
-const getWage = (base, overtime, rate) => {
-  return base + overtime * rate;
-};
-
-console.log(getWage(baseSalary, overtime, rate));
+class Car {}
